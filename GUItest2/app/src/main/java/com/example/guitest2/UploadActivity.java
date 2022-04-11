1573bc992b3ebd1f1607976ed0e5c88d02f679ca
jimleungcc <jimleungcc@gmail.com>
2022-04-11 21:04:22 +0800

    fixed app crash when upload image

--- GUItest2/app/src/main/java/com/example/guitest2/UploadActivity.java
+++ GUItest2/app/src/main/java/com/example/guitest2/UploadActivity.java
@@ -5,50 +5,49 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 
 public class UploadActivity extends AppCompatActivity {
     private ImageButton imageButtonLow, imageButtonHigh;
     int SELECT_PICTURE = 200;
-    private String resolution,lowFileName, highFileName;
-    private String name, description , price;
+    private String resolution, lowFileName, highFileName;
+    private String name, description, price;
     private EditText nameEditText, descriptionEditText, priceEditText;
+    ImageView image;
     // Get the last ImageButton's layout parameters
     RelativeLayout.LayoutParams params;
-
+    Uri selectedImageUri;
+    ArrayList<String> imageData;
     DBHelper dbHelper = new DBHelper(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
-        imageButtonLow = findViewById(R.id.imageButtonLow);
-        imageButtonHigh = findViewById(R.id.imageButtonHight);
+        imageData = new ArrayList<>();
         nameEditText = findViewById(R.id.nameEditText);
         descriptionEditText = findViewById(R.id.descriptionEditText);
         priceEditText = findViewById(R.id.priceEditText);
         Button uploadButton = findViewById(R.id.uploadButton);
 
-        imageButtonLow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resolution = "low";
-                imageChooser();
-            }
-        });
+        image = findViewById(R.id.imageView2);
 
-        imageButtonHigh.setOnClickListener(new View.OnClickListener() {
+        image.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                resolution = "high";
+            public void onClick(View view) {
                 imageChooser();
             }
         });
@@ -60,7 +59,7 @@ public class UploadActivity extends AppCompatActivity {
                 name = nameEditText.getText().toString();
                 description = descriptionEditText.getText().toString();
                 price = priceEditText.getText().toString();
-                dbHelper.insertContact(name, description,price,lowFileName,highFileName);
+                dbHelper.insertContact(name, description, price, lowFileName, highFileName);
                 AlertDialog.Builder builder = new AlertDialog.Builder(UploadActivity.this);
                 builder.setTitle("Uploaded");
                 builder.setMessage("You have uploaded a new item!");
@@ -72,12 +71,19 @@ public class UploadActivity extends AppCompatActivity {
                 });
                 builder.show();
                 //set to default value
-                imageButtonLow.setImageResource(android.R.drawable.ic_menu_gallery);
-                imageButtonHigh.setImageResource(android.R.drawable.ic_menu_gallery);
+//                imageButtonLow.setImageResource(android.R.drawable.ic_menu_gallery);
+//                imageButtonHigh.setImageResource(android.R.drawable.ic_menu_gallery);
                 nameEditText.setText("");
                 descriptionEditText.setText("");
                 priceEditText.setText("");
                 uploadButton.setText("Thanks for support!");
+
+                imageData.add(price);
+                imageData.add(name);
+                imageData.add(description);
+
+                // Back to main menu
+
             }
         });
     }
@@ -100,22 +106,11 @@ public class UploadActivity extends AppCompatActivity {
             // SELECT_PICTURE constant
             if (requestCode == SELECT_PICTURE) {
                 // Get the url of the image from data
-                Uri selectedImageUri = data.getData();
+                selectedImageUri = data.getData();
                 // update the preview image in the layout
                 if (null != selectedImageUri) {
                     //if the image is low resolution picture
-                    if(resolution == "low"){
-                        lowFileName = selectedImageUri.getLastPathSegment();
-                        lowFileName = lowFileName.substring(lowFileName.lastIndexOf("/") + 1);
-                        lowFileName = lowFileName.substring(0, lowFileName.lastIndexOf('.'));
-                        imageButtonLow.setImageURI(selectedImageUri);
-                    }
-                    else{ //if the image is high resolution picture
-                        highFileName = selectedImageUri.getLastPathSegment();
-                        highFileName = highFileName.substring(highFileName.lastIndexOf("/") + 1);
-                        highFileName = highFileName.substring(0, highFileName.lastIndexOf('.'));
-                        imageButtonHigh.setImageURI(selectedImageUri);
-                    }
+                    image.setImageURI(selectedImageUri);
                 }
             }
         }
